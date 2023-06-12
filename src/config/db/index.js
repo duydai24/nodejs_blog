1bfde70411d6fe689894c4c6e690b499ebc503d9
duydai24 <nguyenduydai24@gmail.com>
2023-06-12 08:16:29 +0700

    01

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
-
 async function connect() {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/f8_education_dev',
+    await mongoose.connect('mongodb+srv://duydai24:Opklnm2410@cluster0.dxon8lq.mongodb.net/?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
